7f3cca3adcf0e8f3ea937d22e7b3484300c57d62
Naomi Calabretta <tony0000.ac@gmail.com>
2022-03-09 20:08:28 +0100

    feat(cli): exit if no path is given

--- src/cli.ts
+++ src/cli.ts
@@ -2,6 +2,11 @@ import { resolve } from "path";
 import { patchFile } from ".";
 
 (async () => {
+	if(!process.argv[2]){
+		console.error("You must specify a path to a library as argument!");
+		process.exit(1);
+	}
+	
 	const originalFilePath = resolve(process.argv[2]);
 	const patchedFilePath = await patchFile(originalFilePath);
 	console.log(`Patched file is in: ${patchedFilePath}`);
